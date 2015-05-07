5bc9fa96cd188dfc423f3c93642ddeb5ff6cbb7f
Andrew T. Baker <andrew.tork.baker@gmail.com>
2015-05-06 21:02:57 -0400

    Updating locustfile

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # locust-workshop
 A short, 15-minute workshop on stress-testing web apps with Locust
+
+
+## Notes
+
+Local swarming:
+* 250 users, 25 spawn rate
--- locustfile.py
+++ locustfile.py
@@ -5,23 +5,27 @@ import resource
 class UserBehavior(TaskSet):
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
-        # self.login()
-        resource.setrlimit(resource.RLIMIT_NOFILE, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
-        self.client.headers.update({'Authorization': 'Token 7cdfaa69c2986c0e4c631ba4338c6eed146bb34b'})
+        self.login()
 
-    # def login(self):
-    #     self.client.post("/admin/login", {"username":"admin@tweetcheck.com", "password":"admin"})
+    def login(self):
+        response = self.client.post("/api-token-auth/", {"username": "admin@tweetcheck.com", "password": "admin"})
+        self.client.headers.update({'Authorization': 'Token {0}'.format(response.json()['token'])})
+
+    @task(5)
+    def view_dashboard(self):
+        self.client.get("/api/tweets/")
 
     @task(2)
-    def index(self):
-        self.client.get("/api/tweets")
+    def view_detail(self):
+        self.client.get("/api/tweets/1/")
 
     @task(1)
-    def profile(self):
-        self.client.get("/api/tweets/1")
+    def new_tweet(self):
+        self.client.post("/api/tweets/", {'handle': 1, 'body': 'This tweet brought to you by Locust'})
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     host = 'http://192.168.59.103:8000'
+    # host = 'http://www.tweetcheck.com'
     min_wait = 5000
     max_wait = 9000
